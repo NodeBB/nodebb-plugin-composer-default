095517b5c6ab7b532a3f610ad2ac5d4057b499f1
Peter Jaszkowiak <p.jaszkow@gmail.com>
2016-09-15 17:41:15 -0600

    Remove JS resize functions

--- static/lib/composer/resize.js
+++ static/lib/composer/resize.js
@@ -1,152 +1,154 @@
 
 'use strict';
 
-/* globals define, utils*/
+/* globals define */
 
 define('composer/resize', [], function() {
-	var resize = {},
-		oldPercentage = 0,
-		minimumPercentage = 0.3,
-		snapMargin = $('[component="navbar"]').height() / $(window).height();
+	var resize = {};
+	var oldRatio = 0;
+	var minimumRatio = 0.3;
+	var snapMargin = 0.05;
+	var mediumMin = 992;
 
-	var $body = $('body'),
-		$html = $('html'),
-		$window = $(window),
-		$headerMenu = $('[component="navbar"]');
+	var $body = $('body');
+	var $window = $(window);
+	var $headerMenu = $('[component="navbar"]');
 
+	var html = document.documentElement;
+	var body = document.body;
+	var header = $headerMenu[0];
+
+	function getSavedRatio() {
+		return localStorage.getItem('composer:resizeRatio') || 0.5;
+	}
+
+	function saveRatio(ratio) {
+		localStorage.setItem('composer:resizeRatio', Math.min(ratio, 1));
+	}
+
+	function getBounds() {
+		var headerRect = header.getBoundingClientRect();
+		var rect = html.getBoundingClientRect();
+
+		return {
+			top: rect.top,
+			left: rect.left,
+			right: rect.right,
+			bottom: rect.bottom,
+			width: rect.right - rect.left,
+			height: rect.bottom - rect.top,
+
+			boundedTop: headerRect.bottom,
+			boundedHeight: rect.bottom - headerRect.bottom,
+		};
+	}
+
+	function doResize(postContainer, ratio) {
+		var bounds = getBounds();
+		var elem = postContainer[0];
+
+		if (bounds.width >= mediumMin) {
+			ratio = Math.min(Math.max(ratio, minimumRatio), 1);
+
+			var top = ratio * bounds.boundedHeight / bounds.height;
+			elem.style.top = ((1 - top) * 100).toString() + '%';
+
+			// Add some extra space at the bottom of the body so that 
+			// the user can still scroll to the last post w/ composer open
+			body.style.marginBottom = elem.getBoundingClientRect().top;
+		} else {
+			postContainer.removeAttr('style');
+			body.style.marginBottom = 0;
+		}
+
+		postContainer.ratio = ratio;
+		elem.style.visibility = 'visible';
+	}
+
+	var resizeIt = doResize;
+	var raf = window.requestAnimationFrame ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame;
+
+	if (raf) {
+		resizeIt = function(postContainer, ratio) {
+			raf(function() {
+				doResize(postContainer, ratio);
+			});
+		};
+	}
 
 	resize.reposition = function(postContainer) {
-		var	percentage = localStorage.getItem('composer:resizePercentage') || 0.5;
+		var	ratio = getSavedRatio();
 
-		if (percentage >= 1 - snapMargin) {
-			percentage = 1;
+		if (ratio >= 1 - snapMargin) {
+			ratio = 1;
 			postContainer.addClass('maximized');
 		}
 
-		doResize(postContainer, percentage);
+		resizeIt(postContainer, ratio);
 	};
 
 	resize.maximize = function(postContainer, state) {
 		if (state) {
-			doResize(postContainer, 1);
+			resizeIt(postContainer, 1);
 		} else {
 			resize.reposition(postContainer);
 		}
 	};
 
-	function doResize(postContainer, percentage) {
-		var env = utils.findBootstrapEnvironment();
-
-		if (env === 'sm' || env === 'xs' || window.innerHeight < 480) {
-			$html.addClass('composing mobile');
-			percentage = 1;
-		} else {
-			$html.removeClass('composing mobile');
-		}
-
-		if (percentage) {
-			var upperBound = getUpperBound();
-
-			var windowHeight = $window.height();
-
-			if (percentage < minimumPercentage) {
-				percentage = minimumPercentage;
-			} else if (percentage >= 1) {
-				percentage = 1;
-			}
-
-			if (env === 'md' || env === 'lg') {
-				var top = percentage * (windowHeight - upperBound) / windowHeight;
-				top = (Math.abs(1-top) * 100) + '%';
-				postContainer.css({
-					'top': top
-				});
-			} else {
-				postContainer.removeAttr('style');
-			}
-		}
-
-		postContainer.percentage = percentage;
-		postContainer.css('visibility', 'visible');
-
-		// Add some extra space at the bottom of the body so that the user can still scroll to the last post w/ composer open
-		// thanks but don't do it on mobile
-		if (env === 'md' || env === 'lg') {
-			$body.css({ 'margin-bottom': postContainer.outerHeight() });
-		}
-
-		resizeWritePreview(postContainer);
-	}
-
-	var resizeIt = doResize;
-
-	var raf = window.requestAnimationFrame ||
-					window.webkitRequestAnimationFrame ||
-					window.mozRequestAnimationFrame;
-
-	if (raf) {
-		resizeIt = function(postContainer, percentage) {
-			raf(function() {
-				doResize(postContainer, percentage);
-			});
-		};
-	}
-
 	resize.handleResize = function(postContainer) {
+		var resizeOffset = 0;
+		var resizeBegin = 0;
+		var resizeEnd = 0;
+		var $resizer = postContainer.find('.resizer');
+		var resizer = $resizer[0];
 
 		function resizeStart(e) {
-			var resizeRect = resizeEl[0].getBoundingClientRect(),
-				resizeCenterY = resizeRect.top + (resizeRect.height / 2);
+			var resizeRect = resizer.getBoundingClientRect();
+			var resizeCenterY = (resizeRect.top + resizeRect.bottom) / 2;
 
 			resizeOffset = (resizeCenterY - e.clientY) / 2;
-			resizeActive = true;
-			resizeDown = e.clientY;
+			resizeBegin = e.clientY;
 
 			$window.on('mousemove', resizeAction);
 			$window.on('mouseup', resizeStop);
 			$body.on('touchmove', resizeTouchAction);
 		}
 
+		function resizeAction(e) {
+			var position = e.clientY - resizeOffset;
+			var bounds = getBounds();
+			var ratio = (bounds.height - position) / (bounds.boundedHeight);
+
+			resizeEnd = e.clientY;
+
+			resizeIt(postContainer, ratio);
+		}
+
 		function resizeStop(e) {
-			resizeActive = false;
+			e.preventDefault();
 
 			postContainer.find('textarea').focus();
 			$window.off('mousemove', resizeAction);
 			$window.off('mouseup', resizeStop);
 			$body.off('touchmove', resizeTouchAction);
 
-			var position = (e.clientY - resizeOffset),
-				windowHeight = $window.height(),
-				upperBound = getUpperBound(),
-				newHeight = windowHeight - position,
-				ratio = newHeight / (windowHeight - upperBound);
+			var position = resizeEnd - resizeOffset;
+			var bounds = getBounds();
+			var ratio = (bounds.height - position) / (bounds.boundedHeight);
 
-			if (ratio >= 1 - snapMargin) {
-				snapToTop = true;
+			saveRatio(ratio);
+			
+			if (resizeEnd - resizeBegin === 0 && postContainer.hasClass('maximized')) {
+				postContainer.removeClass('maximized');
+				var newRatio = (!oldRatio || oldRatio >= 1 - snapMargin) ? 0.5 : oldRatio;
+				resizeIt(postContainer, newRatio);
+			} else if (resizeEnd - resizeBegin === 0 || ratio >= 1 - snapMargin) {
+				resizeIt(postContainer, 1);
+				postContainer.addClass('maximized');
 			} else {
-				snapToTop = false;
-			}
-
-			resizeSavePosition(ratio);
-
-			toggleMaximize(e);
-		}
-
-		function toggleMaximize(e) {
-			if (e.clientY - resizeDown === 0 || snapToTop) {
-				var newPercentage = 1;
-
-				if (!postContainer.hasClass('maximized') || !snapToTop) {
-					oldPercentage = postContainer.percentage;
-					resizeIt(postContainer, newPercentage);
-					postContainer.addClass('maximized');
-				} else {
-					newPercentage = (oldPercentage >= 1 - snapMargin || oldPercentage == 0) ? 0.5 : oldPercentage;
-					resizeIt(postContainer, newPercentage);
-					postContainer.removeClass('maximized');
-				}
-
-				resizeSavePosition(newPercentage);
+				postContainer.removeClass('maximized');
 			}
 		}
 
@@ -155,84 +157,14 @@ define('composer/resize', [], function() {
 			resizeAction(e.touches[0]);
 		}
 
-		function resizeAction(e) {
-			if (resizeActive) {
-				var position = (e.clientY - resizeOffset),
-					windowHeight = $window.height(),
-					upperBound = getUpperBound(),
-					newHeight = windowHeight - position,
-					ratio = newHeight / (windowHeight - upperBound);
-
-				resizeIt(postContainer, ratio);
-
-				resizeWritePreview(postContainer);
-
-				if (Math.abs(e.clientY - resizeDown) > 0) {
-					postContainer.removeClass('maximized');
-				}
-			}
-
-			e.preventDefault();
-			return false;
-		}
-
-		function resizeSavePosition(percentage) {
-			localStorage.setItem('composer:resizePercentage', percentage <= 1 ? percentage : 1);
-		}
-
-		var	resizeActive = false,
-			resizeOffset = 0,
-			resizeDown = 0,
-			snapToTop = false,
-			resizeEl = postContainer.find('.resizer');
-
-		resizeEl
+		$resizer
 			.on('mousedown', resizeStart)
 			.on('touchstart', function(e) {
 				e.preventDefault();
 				resizeStart(e.touches[0]);
 			})
-			.on('touchend', function(e) {
-				e.preventDefault();
-				resizeStop();
-			});
+			.on('touchend', resizeStop);
 	};
-
-	function getUpperBound() {
-		try {
-			var rect = $headerMenu.get(0).getBoundingClientRect();
-			return rect.height + rect.top;
-		} catch (e) {
-			return 0;
-		}
-	}
-
-	function resizeWritePreview(postContainer) {
-		var total = getFormattingHeight(postContainer),
-			containerHeight = postContainer.height() + 20 - total;
-
-		postContainer
-			.find('.write-preview-container')
-			.css('height', containerHeight);
-
-		$window.trigger('action:composer.resize', {
-			formattingHeight: total,
-			containerHeight: containerHeight
-		});
-	}
-
-	function getFormattingHeight(postContainer) {
-		return [
-			postContainer.find('.title-container').outerHeight(true),
-			postContainer.find('.formatting-bar').outerHeight(true),
-			postContainer.find('.topic-thumb-container').outerHeight(true) || 0,
-			postContainer.find('.tag-row').outerHeight(true) || 0,
-			$('.taskbar').height() || 50
-		].reduce(function(a, b) {
-			return a + b;
-		});
-	}
-
 
 	return resize;
 });
