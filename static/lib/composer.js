8a5ce7ba04facf8478baa58db94911abbb0cf484
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2017-09-23 19:34:33 +0200

    Fix parentheses

--- static/lib/composer.js
+++ static/lib/composer.js
@@ -314,7 +314,7 @@ define('composer', [
 		
 		postContainer.keypress(function (event) {                                 
 			var keyCode = (event.which ? event.which : event.keyCode);
-			if (keyCode === 10 || keyCode == 13 && event.ctrlKey) {
+			if ((keyCode === 10 || keyCode == 13) && event.ctrlKey) {
 				submitBtn.attr('disabled', true);
 				post(post_uuid);
 				return false;
