443f8c47b246ae8cc13917a932aaa44ede2dc659
Barış Soner Uşaklı <barisusakli@gmail.com>
2023-08-24 12:53:52 -0400

    dont prevent clicks

--- static/lib/composer/post-queue.js
+++ static/lib/composer/post-queue.js
@@ -4,12 +4,14 @@ define('composer/post-queue', [], function () {
 	const postQueue = {};
 
 	postQueue.showAlert = async function (postContainer, postData) {
+		const alertEl = postContainer.find('[component="composer/post-queue/alert"]')
 		if (!config.postQueue || app.user.isAdmin || app.user.isGlobalMod || app.user.isMod) {
+			alertEl.remove();
 			return;
 		}
 		const shouldQueue = await socket.emit('plugins.composer.shouldQueue', { postData: postData });
-		postContainer.find('[component="composer/post-queue/alert"]')
-			.toggleClass('show', shouldQueue);
+		alertEl.toggleClass('show', shouldQueue);
+		alertEl.toggleClass('pe-none', !shouldQueue);
 	};
 
 	postQueue.onChangeCategory = async function (postContainer, postData) {
