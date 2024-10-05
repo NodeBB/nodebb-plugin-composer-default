2a15890bd749e0f9e4f4ca1bde00e6b45ae0fc35
Bchenjamin <bzchen@andrew.cmu.edu>
2024-10-05 14:58:56 -0400

    Added listener for the anonymous checkbox, send checked data to main repo

--- static/lib/composer.js
+++ static/lib/composer.js
@@ -665,6 +665,7 @@ define('composer', [
 		var titleEl = postContainer.find('.title');
 		var bodyEl = postContainer.find('textarea');
 		var thumbEl = postContainer.find('input#topic-thumb-url');
+		var anonymousEl = postContainer.find('#anonymousPost');
 		var onComposeRoute = postData.hasOwnProperty('template') && postData.template.compose === true;
 		const submitBtn = postContainer.find('.composer-submit');
 
@@ -715,6 +716,10 @@ define('composer', [
 			return composerAlert(post_uuid, '[[error:scheduling-to-past]]');
 		}
 
+		// Check if the anonymous checkbox was filled in or not
+		var isAnonymous = anonymousEl.is(':checked');
+		console.log(isAnonymous);
+
 		let composerData = {
 			uuid: post_uuid,
 		};
@@ -722,6 +727,7 @@ define('composer', [
 		let route = '';
 
 		if (action === 'topics.post') {
+			console.log('topics.post called');
 			route = '/topics';
 			composerData = {
 				...composerData,
@@ -732,6 +738,7 @@ define('composer', [
 				cid: categoryList.getSelectedCid(),
 				tags: tags.getTags(post_uuid),
 				timestamp: scheduler.getTimestamp(),
+				anonymous: isAnonymous, // Add anonymous flag
 			};
 		} else if (action === 'posts.reply') {
 			route = `/topics/${postData.tid}`;
@@ -741,6 +748,7 @@ define('composer', [
 				handle: handleEl ? handleEl.val() : undefined,
 				content: bodyEl.val(),
 				toPid: postData.toPid,
+				anonymous: isAnonymous, // Add anonymous flag here too
 			};
 		} else if (action === 'posts.edit') {
 			method = 'put';
@@ -754,6 +762,7 @@ define('composer', [
 				thumb: thumbEl.val() || '',
 				tags: tags.getTags(post_uuid),
 				timestamp: scheduler.getTimestamp(),
+				anonymous: isAnonymous, // Add anonymous flag here as well
 			};
 		}
 		var submitHookData = {
