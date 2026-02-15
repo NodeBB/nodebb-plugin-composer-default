NodeBB/nodebb-plugin-composer-default

commit 73e680688ced64c47afa2a5a2bc22adb02115b5e

closes https://github.com/julianlam/nodebb-plugin-mentions/issues/28

static/lib/composer.js
@@ -127,7 +127,15 @@ define('composer', [
 
 		if (existingUUID) {
 			taskbar.updateActive(existingUUID);
-			return composer.load(existingUUID);
+			if (post.body) {
+				const postContainer = $('.composer[data-uuid="' + existingUUID + '"]');
+				const bodyEl = postContainer.find('textarea');
+				const prevText = bodyEl.val();
+				composer.posts[existingUUID].body = (prevText.length ? prevText + '\n\n' : '') + post.body;
+				bodyEl.val(composer.posts[existingUUID].body);
+				preview.render(postContainer);
+			}
+			return;
 		}
 
 		var actionText = '[[topic:composer.new-topic]]';

commit be60ba6f30831f339efa6322edb3a4743d9421db

fix: regression from https://github.com/NodeBB/nodebb-plugin-composer-default/commit/73e680688ced64c47afa2a5a2bc22adb02115b5e

static/lib/composer.js
@@ -135,7 +135,7 @@ define('composer', [
 				bodyEl.val(composer.posts[existingUUID].body);
 				preview.render(postContainer);
 			}
-			return;
+			return composer.load(existingUUID);
 		}
 
 		var actionText = '[[topic:composer.new-topic]]';